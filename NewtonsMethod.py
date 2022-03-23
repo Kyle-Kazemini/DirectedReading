4c609c9cdf6b4da50eacc366591b575817da9a53
Kyle-Kazemini <59711345+Kyle-Kazemini@users.noreply.github.com>
2022-03-23 01:07:32 -0600

    Moved Newton's method functions to new file.

---
+++ NewtonsMethod.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+
+def newtons_method(f, Df, w_0, N, epsilon=1e-5):
+    """
+    Implementation of Newton's method for finding
+    the roots of a function.
+    :param f: the function we want to find the roots of
+    :param Df: derivative of f
+    :param w_0: initial guess
+    :param epsilon: tolerance
+    :param N: number of iterations
+    :return: root of f
+    """
+
+    w = w_0
+
+    for i in range(N):
+        if np.abs(f(w)) < epsilon:
+            return w
+
+        w = w - (f(w) / Df(w))
+
+    print("No solution found after max number of iterations.")
+    return w
+
+
+def nd_newtons_method(f, J, w_0, N, epsilon=1e-5):
+    """
+    Implementation of Newton's method for finding
+    the roots of a multi-dimensional function.
+    :param f: the function we want to find the roots of
+    :param J: Jacobian of f
+    :param w_0: initial guess
+    :param epsilon: tolerance
+    :param N: number of iterations
+    :return: root of f
+    """
+
+    i = 0
+    w = w_0
+    val = f(w)
+    norm = np.linalg.norm(val, ord=2)
+
+    while i < N and abs(norm) > epsilon:
+        delta = np.linalg.solve(J(w), val)
+        w = w + delta
+        val = f(w)
+        norm = np.linalg.norm(val, ord=2)
+        i += 1
+
+    if abs(norm) > epsilon:
+        i = -1
+
+    return w
+
+
+
